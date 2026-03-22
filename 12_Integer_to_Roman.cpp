a22f4798539dfa3f02bb1d39b5bb4e3954b793f7
AaryaKale0607 <kaleaarya704@gmai.com>
2026-03-22 23:10:12 +0530

    Updated Integer to Roman solution

--- 12_Integer_to_Roman.cpp
+++ 12_Integer_to_Roman.cpp
@@ -10,6 +10,6 @@ public:
         string hundreds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         string thousands[] = {"","M","MM","MMM"};
 
-        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] +            units[num%10];
+        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + units[num%10];
     }
 };
