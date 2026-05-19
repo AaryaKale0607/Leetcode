0dc99c2ee9d1bcb965e0967f047dbfa43e17ba63
AaryaKale0607 <kaleaarya704@gmail.com>
2026-05-19 21:26:38 +0530

    Given a string s consisting of words and spaces, return the length of the last word in the string.

---
+++ 58_Length_of_Last_Word.cpp
@@ -0,0 +1,24 @@
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+         int i = s.length() - 1;
+        
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        
+        int len = 0;
+        
+        // Count characters of last word
+        while (i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+        
+        return len;
+    }
+};
