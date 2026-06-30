6bfc977674be5e2c1c7868ae6ba3c0f324860cf8
AaryaKale0607 <kaleaarya704@gmail.com>
2026-06-30 16:14:57 +0530

    Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.

---
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,41 @@
+#include<unordered_map>
+#include<vector>
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+
+class Solution {
+public:
+      unordered_map<int, int> inorderIndex;
+    int preorderPos = 0;
+
+    TreeNode* build(vector<int>& preorder, int left, int right) {
+        if (left > right)
+            return nullptr;
+
+        int rootVal = preorder[preorderPos++];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int index = inorderIndex[rootVal];
+
+        root->left = build(preorder, left, index - 1);
+        root->right = build(preorder, index + 1, right);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+          for (int i = 0; i < inorder.size(); i++) {
+            inorderIndex[inorder[i]] = i;
+        }
+
+        return build(preorder, 0, inorder.size() - 1);
+    }
+};
